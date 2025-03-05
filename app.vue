c424cde1736a004e35abd10845635d54e83236c9
Vivelis <maceo.jalbert@gmail.com>
2025-03-05 22:42:43 +0900

    Add versel analytics

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt';
+</script>
+
 <template>
   <div>
     <!-- Markup shared across all pages, ex: NavBar -->
+    <Analytics />
     <NuxtPage />
   </div>
 </template>
