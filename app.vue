b1a2afaf3a2401d7c60b38e81ae5817fe393fc9e
Vivelis <maceo.jalbert@gmail.com>
2025-03-05 22:44:06 +0900

    Add vercel speed insight

--- app.vue
+++ app.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { Analytics } from '@vercel/analytics/nuxt';
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 </script>
 
 <template>
   <div>
     <!-- Markup shared across all pages, ex: NavBar -->
     <Analytics />
+    <SpeedInsights />
     <NuxtPage />
   </div>
 </template>
