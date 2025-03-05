e60d5e0cfa2bda99f22ba5157a284fdab3ac55ef
Vivelis <maceo.jalbert@gmail.com>
2025-03-05 22:31:06 +0900

    Update  portfolio to use nuxtjs

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const route = useRoute()
+const { data: page } = await useAsyncData(route.path, () => {
+  return queryCollection('content').path(route.path).first()
+})
+</script>
+
+<template>
+  <div>
+    <header><!-- ... --></header>
+
+    <ContentRenderer v-if="page" :value="page" />
+
+    <footer><!-- ... --></footer>
+  </div>
+</template>
